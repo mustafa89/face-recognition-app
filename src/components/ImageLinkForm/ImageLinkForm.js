7826c88ce4d71918c4db17cedf51b5311a861b38
mustafa89 <mustafa.mujahid@outlook.com>
2020-12-26 08:17:12 +0100

    :sparkles: ImageLinkForm: Add the component for the image link form

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
     return (
-        <div className='ma4 '>
-               <img src={brain} alt="logo"/>
+        <div>
+            <p className="white f3">
+                {'This app will detect faces in your pictures. Give it a try.'}
+            </p>
+            <div className='flex justify-center'>
+            <div className='form shadow-5 ma2'>
+                <input className='f4 br2 pa3 ma3 mr0 w-50' type="text" onChange={onInputChange}/>
+                <button className='dark-gray w-20 pa3 ma1 bg-red grow hover-bg-dark-red ba b--moon-gray br3 white'>
+                    {'Detect'}
+                </button>
+            </div>
+            </div>
         </div>
     )
 }
