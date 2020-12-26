a8cf5a312bae7d3cd89e7af5f4b83429fe21ac0b
mustafa89 <mustafa.mujahid@outlook.com>
2020-12-26 08:18:10 +0100

    :sparkles: Rank: Add component for Rank and also particle js for the cool background animation

--- src/App.js
+++ src/App.js
@@ -3,18 +3,46 @@ import './App.css'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 import 'tachyons'
+import Particles from 'react-particles-js';
+import particle_params from './particles'
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
   render () {
     return (
       <div className='App'>
+        <Particles className='particles' params={particle_params} />
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
       {/*<FaceRecognition />} */}
       </div>
     )
   }
 }
+
+// const options = { 
+//   particles: {
+//     line_linked: {
+//       shadow: {
+//         enable: true,
+//         color: "#3CA9D1",
+//         blur: 5
+//       }
+//     }
+//   }
+// }
 
 export default App
---
+++ src/particles.js
@@ -0,0 +1,107 @@
+const particle_params = {
+    "particles": {
+      "number": {
+        "value": 104,
+        "density": {
+          "enable": true,
+          "value_area": 710.2328774690454
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        }
+      },
+      "opacity": {
+        "value": 0.12626362266116362,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 4.008530152163807,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#3b841c",
+        "opacity": 0.7733646887996272,
+        "width": 1.1048066982851816
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "right",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+  export default particle_params
